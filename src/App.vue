2577946ff63e4c87b937bb26bad16052f2a34b33
Xindong Lin <xindong.lin@sap.com>
2022-10-06 14:31:51 -0700

    update day2

--- src/App.vue
+++ src/App.vue
@@ -4,39 +4,45 @@
       <ul>
         <li class="nav-item">
           <img class="logo" src="./assets/build-a-bot-logo.png">
+          Build a bot
         </li>
       </ul>
     </nav>
   </header>
   <main>
-    <HomePage />
+    <Search />
   </main>
 </template>
 
 <script>
-import HomePage from './components/HomePage.vue';
+// import HomePage from './home/HomePage.vue';
+// import RobotBuilder from './build/RobotBuilder.vue';
+import Search from './search/Search.vue';
 
 export default {
   name: 'App',
   components: {
-    HomePage,
+    Search,
   },
 };
 </script>
 
 <style>
-body {
-  background: linear-gradient(to bottom, #555, #999);
-  background-attachment: fixed;
-}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    background: linear-gradient(to bottom, #555, #999);
+    background-attachment: fixed;
+  }
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
+
+<style scoped>
 main {
   margin: 0 auto;
   padding: 30px;
